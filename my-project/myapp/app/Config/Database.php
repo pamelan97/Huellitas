3ad331098905d0ba5a238356286545098213ab37
YobanaV <102844680+YobanaV@users.noreply.github.com>
2022-04-08 20:09:12 -0400

    modificacion pasword DB

--- my-project/myapp/app/Config/Database.php
+++ my-project/myapp/app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends Config
         'DSN'      => '',
         'hostname' => 'localhost',
         'username' => 'root',
-        'password' => 'ddd',
+        'password' => 'aaa',
         'database' => 'Huellitas',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
