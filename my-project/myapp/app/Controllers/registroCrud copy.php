63eea1a77ac770ae529e79a307ec4ab769a6d908
pamelan97 <102864377+pamelan97@users.noreply.github.com>
2022-04-18 21:04:34 -0400

    avance qr pame

---
+++ my-project/myapp/app/Controllers/registroCrud copy.php
@@ -0,0 +1,121 @@
+<?php 
+namespace App\Controllers;
+//
+use CodeIgniter\Controller;
+//
+use App\Models\registroModel;
+use App\Entities\Registro;
+use App\Models\UsuarioModel;
+//
+class registroCrud extends Controller
+{
+public function index(){
+    // Obtenemos la clase del Model que controla los conciertos
+    $mod = new registroModel();
+    // Buscamos los conciertos
+    $usuarios = $mod->todEs();
+    // UN EJEMPLO PARA MAS ADELANTE
+    //$conciertos = $mod->soloConA();
+    
+    // Ponemos en la 'data transiente' la data que queremos mostrar
+    $data['registros'] = $usuarios;
+    // Vamos a la vista ... pero con los datos!!!
+    return view('registro/lista',$data);
+}
+
+private function recuperauser($unId){
+    // Obtenemos la clase del Model que controla los conciertos
+    $mod = new registroModel();
+    // Buscamos el Concierto por la PK
+    $usuarios = $mod->find($unId);
+    // Dejamos el concierto la 'data transiente'
+    $laData['registro'] = $usuarios;
+    return $laData;
+}
+
+public function ver($id){
+    // 
+    $data = $this->recuperauser($id);
+    //Vamos a la vista
+    return view('registro/ver',$data);
+}
+
+public function agregar01Formulario(){
+    return view('registro/agregar01Formulario');
+}
+
+public function agregar02Continuar(){
+    
+    
+    $model = new UsuarioModel();
+    $unCorreo = $this->request->getVar('correo');
+        $usuario = $model->usuarioPorCorreo($unCorreo);
+        // **$clave = $model->usuarioPorclave($unaClave);
+
+        if (sizeof($usuario) !=1 ) {
+             // Recuperamos los datos desde el formulario (porque se enviaron por un POST y Request)
+    $unRegistro = new registro();
+    $unRegistro->correo =  $this->request->getVar('correo');
+    $unRegistro->clave =  $this->request->getVar('clave');
+    // Obtenemos la clase del Model que controla los conciertos
+    $mod = new registroModel();
+    // MAndamos la Transacciòn ala Base de DAtos
+    $mod->save($unRegistro);
+    //
+    return $this->index();
+    
+        }
+        else{
+           
+            echo "Disculpe, ya esta registrado";
+            return view('registro/agregar01Formulario'); 
+           
+            
+        }
+    
+   
+}
+
+public function editar01Formulario($id){
+    $data = $this->recuperauser($id);
+    //Vamos a la vista
+    return view('registro/editar01Formulario',$data);
+}
+
+public function editar02Continuar(){
+    // Recuperamos los datos desde el formulario (porque se enviaron por un POST y Request)
+    $unRegistro = new registro();
+    $unRegistro->id = $this->request->getVar('id');
+    $unRegistro->correo =  $this->request->getVar('correo');
+    $unRegistro->clave =  $this->request->getVar('clave');
+    // Obtenemos la clase del Model que controla los conciertos
+    $mod = new registroModel();
+    // Mandamos la Transacciòn ala Base de DAtos
+    $mod->actualziar($unRegistro);
+    // vuelve al inicio
+   return $this->index();
+}
+
+public function eliminar01Formulario($id){
+    $data = $this->recuperauser($id);
+    //Vamos a la vista
+    return view('registro/eliminar',$data);
+}
+
+public function eliminar02Continuar(){
+     // Recuperamos los datos desde el formulario (porque se enviaron por un POST y Request)
+     $unRegistro = new registro();
+     $unRegistro->id = $this->request->getVar('id');
+     // Obtenemos la clase del Model que controla los conciertos
+     $mod = new registroModel();
+     // Mandamos la Transacciòn ala Base de DAtos
+     $mod->eliminar($unRegistro);   
+     //Home
+     return $this->index();
+ }
+
+ public function cancelar(){
+    return $this->index();
+ }
+
+}
