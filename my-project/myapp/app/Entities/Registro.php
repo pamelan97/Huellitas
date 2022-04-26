84773c6cfb0411feb494994db0f76fbad935d973
YobanaV <102844680+YobanaV@users.noreply.github.com>
2022-04-25 22:51:59 -0400

    Modificacion Entity y Model

--- my-project/myapp/app/Entities/Registro.php
+++ my-project/myapp/app/Entities/Registro.php
@@ -1,23 +1,35 @@
 <?php
 
+
+
 namespace App\Entities;
+
+
 
 use CodeIgniter\Entity\Entity;
 
+
+
 class Registro extends Entity
+
 {
 
+
+
     protected $datamap = [
+
         "id" => "usuario_id",
+
         "correo" => "usuario_correo",
+
         "clave" => "usuario_clave",
-        "telefono1" => "usuario_telefono1",
-        "telefono2" => "usuario_telefono2",
-        "nombreMascota" => "usuario_nombre_mascota",
-        "fechaMascota" => "usuario_nacimiento_mascota",
-        "razaMascota" => "usuario_raza_mascota",
+
     ];
 
+
+
     protected $casts   = [];
+
 }
+
 ?>
---
+++ my-project/myapp/app/Entities/RegistroMascotaEntity.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+namespace App\Entities;
+
+
+
+use CodeIgniter\Entity\Entity;
+
+
+
+class RegistroMascotaEntity extends Entity
+
+{
+
+
+
+    protected $datamap = [
+
+        "id" => "usuarioRegistro_id",
+
+        "telefono1" => "usuario_telefono1",
+
+        "telefono2" => "usuario_telefono2",
+
+        "nombreMascota" => "usuario_nombre_mascota",
+
+        "fechaMascota" => "usuario_nacimiento_mascota",
+
+        "razaMascota" => "usuario_raza_mascota",
+
+     
+
+    ];
+
+
+
+    protected $casts   = [];
+
+}
+
+?>
