a9b0bb8d60bdd8209f21ea052302e965046cb52f
YobanaV <102844680+YobanaV@users.noreply.github.com>
2022-05-30 20:35:51 -0400

    script

---
+++ Scriptbasedatos/004_estadoMascota.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `estadoMascotas` (
+  `estadoMascotas_id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`estadoMascotas_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=124 DEFAULT CHARSET=utf8;
