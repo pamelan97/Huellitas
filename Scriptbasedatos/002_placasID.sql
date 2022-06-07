4d17188f466a315cfaa52b11e4d3467dde793aa1
pamelan97 <102864377+pamelan97@users.noreply.github.com>
2022-06-07 01:22:49 -0400

    script bd

--- Scriptbasedatos/002_placasID.sql
+++ Scriptbasedatos/002_placasID.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `placasMascotas` (
   `placasMascotas_id` int(11) NOT NULL AUTO_INCREMENT,
-  `usuarioRegistro_id` int(11) DEFAULT NULL,
+  `registroMascota_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`placasMascotas_id`),
-  KEY `placasMascotas_FK` (`usuarioRegistro_id`)
+  KEY `placasMascotas_FK` (`registroMascota_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=124 DEFAULT CHARSET=utf8;
--- Scriptbasedatos/003_mascotaREGISTRO.sql
+++ Scriptbasedatos/003_mascotaREGISTRO.sql
@@ -1,6 +1,6 @@
-CREATE TABLE `usuarioRegistro1` (
+CREATE TABLE `registroMascota` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `usuarioRegistro_id` int(11) NOT NULL,
+  `registroMascota_id` int(11) NOT NULL,
   `usuario_telefono1` varchar(300) NOT NULL,
   `usuario_telefono2` varchar(300) NOT NULL,
   `usuario_nombre_mascota` varchar(300) NOT NULL,
@@ -8,11 +8,15 @@ CREATE TABLE `usuarioRegistro1` (
   `usuario_raza_mascota` int(11) NOT NULL,
   `usuario_tamano_mascota` int(11) NOT NULL,
   `usuario_genero_mascota` int(11) NOT NULL,
+  `estadoMascotas` int(11) NOT NULL DEFAULT 2,
+  `mensaje` varchar(300) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `usuarioRegistro1_FK` (`usuario_genero_mascota`),
-  KEY `usuarioRegistro1_FK_1` (`usuario_raza_mascota`),
-  KEY `usuarioRegistro1_FK_2` (`usuario_tamano_mascota`),
-  CONSTRAINT `usuarioRegistro1_FK` FOREIGN KEY (`usuario_genero_mascota`) REFERENCES `genero_mascota` (`genero_id`),
-  CONSTRAINT `usuarioRegistro1_FK_1` FOREIGN KEY (`usuario_raza_mascota`) REFERENCES `raza_mascota` (`raza_id`),
-  CONSTRAINT `usuarioRegistro1_FK_2` FOREIGN KEY (`usuario_tamano_mascota`) REFERENCES `tamano_mascota` (`tamano_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8;
+  KEY `registroMascota_FK` (`usuario_genero_mascota`),
+  KEY `registroMascota_FK_1` (`usuario_raza_mascota`),
+  KEY `registroMascota_FK_2` (`usuario_tamano_mascota`),
+  KEY `registroMascota_FK_3` (`estadoMascotas`),
+  CONSTRAINT `registroMascota_FK` FOREIGN KEY (`usuario_genero_mascota`) REFERENCES `genero_mascota` (`genero_id`),
+  CONSTRAINT `registroMascota_FK_1` FOREIGN KEY (`usuario_raza_mascota`) REFERENCES `raza_mascota` (`raza_id`),
+  CONSTRAINT `registroMascota_FK_2` FOREIGN KEY (`usuario_tamano_mascota`) REFERENCES `tamano_mascota` (`tamano_id`),
+  CONSTRAINT `registroMascota_FK_3` FOREIGN KEY (`estadoMascotas`) REFERENCES `estadoMascotas` (`estadoMascotas_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=77 DEFAULT CHARSET=utf8;
