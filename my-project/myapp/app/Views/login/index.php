a014b005f29fdcedafc855326fded8fe5fd47e1b
pamelan97 <102864377+pamelan97@users.noreply.github.com>
2022-06-14 21:18:12 -0400

    ahora si

--- my-project/myapp/app/Views/login/index.php
+++ my-project/myapp/app/Views/login/index.php
@@ -8,33 +8,6 @@
   <div class="d-flex justify-content-end ">
   <a href="<?php echo site_url('/registromascotaindex') ?>" class="btn btn-info">Refrescar</a>
         
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-
 	</div>
 <div class="mt-3">
      <table class="table table-bordered" id="users-list">
@@ -78,6 +51,9 @@
               <a href="<?php echo site_url('/registromascota-editar/'.$reg['mascota']->id);?>" class="btn btn-primary">Editar</a><br><br>
               <a href="<?php echo site_url('/registromascota-eliminar/'.$reg['mascota']->id);?>" class="btn btn-danger">Eliminar</a><br><br>
               <a href="<?php echo site_url('/mascotaperdida/'.$reg['mascota']->id);?>" class="btn btn-warning btn-sm"><?= ($reg['mascota']->estadoMascotas==1?'Mascota Encontrada':'Mascota Perdida') ?></a><br><br>
+             
+             
+             
               <?php if($reg['mensaje']):?>
                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#mensaje<?=$reg['mascota']->id?>">
                   ver mensajes
@@ -103,6 +79,9 @@
                   </div>
                 </div>
         
+
+
+
               <?php endif; ?>
             </td>
           </tr>
